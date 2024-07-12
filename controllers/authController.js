4f3b900cfb149bb35377c647706fa7bc319b1c3c
Irumva Shumbusho <102556884+kent250@users.noreply.github.com>
2024-07-12 15:38:53 +0300

    Implement Role Based Authentication

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,9 @@
 const User = require('../models/user');
-const brcrypt = require('bcrypt');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const { secret_key, expiresIn } = require('../config/jwt');
+
+
 
 
 const registerBuyer = async (req, res) => {
@@ -7,7 +11,7 @@ const registerBuyer = async (req, res) => {
         const { name, email, password} = req.body;
         
         //hash password
-        const hashedPswd = await brcrypt.hash(password, 10);
+        const hashedPswd = await bcrypt.hash(password, 10);
 
         // Create a new user
         const newUser = await User.create({
@@ -25,6 +29,51 @@ const registerBuyer = async (req, res) => {
       }
 }
 
+
+
+const login = async (req, res)=>{
+    
+        try {
+          const { email, password } = req.body;
+          const user = await User.findOne({ where: { email } });
+      
+          if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+          }
+      
+          const isPasswordValid = await bcrypt.compare(password, user.password);
+      
+          if (!isPasswordValid) {
+            return res.status(401).json({ message: 'Invalid password' });
+          }
+      
+          // Include the user's role in the JWT
+          const token = jwt.sign({ id: user.id, email: user.email, role: user.role }, secret_key, { expiresIn });
+      
+          res.json({ token });
+        } catch (error) {
+          res.status(500).json({ message: 'Error logging in', error: error.message });
+        }
+}
+
+
+const admin = async (req, res)=> {
+  res.json({message: 'admin protected'});
+}
+const all = async (req, res)=> {
+  res.json({message: 'all auntenticated'});
+}
+const buyer = async (req, res)=> {
+  res.json({message: 'buyer'});
+}
+
+
+
+
 module.exports = {
-    registerBuyer
+    registerBuyer,
+    login,
+    admin,
+    all,
+    buyer
 }
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,11 +1,24 @@
 const express = require('express')
 const router = express.Router();
 const authController = require('../controllers/authController');
+const { authenticateToken, authorizeRole } = require('../middlewares/auth');
+const bcrypt = require('bcrypt');  
 
 
 
 
 router.post('/api/auth/register', authController.registerBuyer);
+router.post('/api/auth/login', authController.login);
+
+router.get('/api/auth/all',authenticateToken ,authController.all);
+router.get('/api/auth/buyer',authenticateToken, authorizeRole(['buyer']),authController.buyer);
+router.get('/api/auth/admin',authenticateToken, authorizeRole(['admin']),authController.admin);
+
+
+
+
+
+
 
 
 
