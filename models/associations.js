b0e4116dcfba31d3a34ba2902dfcc94998e89d03
Irumva Shumbusho <102556884+kent250@users.noreply.github.com>
2024-07-13 13:38:05 +0300

    Update associations.js

--- models/associations.js
+++ models/associations.js
@@ -1,6 +1,6 @@
 const Product = require('./product');
 const Category = require('./category');
-const { RelationshipType } = require('sequelize/lib/errors/database/foreign-key-constraint-error');
+
 
 const setupAssociations = () => {
 
