1308c593c863586ef5b29e8849bfca67d369cad0
Irumva Shumbusho <102556884+kent250@users.noreply.github.com>
2024-07-14 13:46:41 +0300

    add some order files

--- models/associations.js
+++ models/associations.js
@@ -1,5 +1,7 @@
 const Product = require('./product');
 const Category = require('./category');
+const Order = require('./order');
+const User = require('./user');
 
 
 const setupAssociations = () => {
@@ -7,6 +9,10 @@ const setupAssociations = () => {
     //product-category relationship 
     Product.belongsTo(Category, { foreignKey: 'category_id', as: 'category' });
     Category.hasMany(Product, { foreignKey: 'category_id', as: 'products' });
+
+    //user-order relationship 
+    Order.belongsTo(User, { foreignKey: 'buyer_id', as: 'buyer' });
+    User.hasMany(Order, { foreignKey: 'buyer_id', as: 'orders' });
 };
 
 module.exports = setupAssociations;
