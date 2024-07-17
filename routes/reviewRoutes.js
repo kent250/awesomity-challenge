59aae3234cfa6a758b8fb1eff19061763af3e7fd
Irumva Shumbusho <102556884+kent250@users.noreply.github.com>
2024-07-18 01:22:01 +0300

    add retrieve single product reviews

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -3,7 +3,10 @@ const router = express.Router();
 const reviewController = require('../controllers/reviewController');
 const { authenticateToken, authorizeRole } = require('../middlewares/auth');
 
-
+//create a review
 router.post('/reviews', authenticateToken, authorizeRole(['buyer']), reviewController.createReview);
+
+//get reviews for a single product
+router.get('/reviews/product/:productId', reviewController.retrieveReviews);
 
 module.exports = router;
