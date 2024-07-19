0267e2f1297ea156bce717b80812f6c6de4548cc
Irumva Shumbusho <102556884+kent250@users.noreply.github.com>
2024-07-19 11:41:09 +0300

    Fix make  order

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -6,13 +6,13 @@ const { authenticateToken, authorizeRole } = require('../middlewares/auth');
 
 router.post('/orders/', authenticateToken, authorizeRole(['buyer']) , orderController.makeOrder);
 
-router.get('/orders/', authenticateToken, authorizeRole(['buyer', 'admin']) , orderController.retrieveOrders);
+// router.get('/orders/', authenticateToken, authorizeRole(['buyer', 'admin']) , orderController.retrieveOrders);
 
-router.get('/orders/:id(\\d+)', authenticateToken, authorizeRole(['buyer', 'admin']), orderController.orderDetails);
+// router.get('/orders/:id(\\d+)', authenticateToken, authorizeRole(['buyer', 'admin']), orderController.orderDetails);
 
-router.patch('/orders/:id', authenticateToken, authorizeRole(['admin']), orderController.updateOrderStatus);
+// router.patch('/orders/:id', authenticateToken, authorizeRole(['admin']), orderController.updateOrderStatus);
 
-router.get('/orders/history', authenticateToken, authorizeRole(['buyer']), orderController.viewOrderHistory);
+// router.get('/orders/history', authenticateToken, authorizeRole(['buyer']), orderController.viewOrderHistory);
 
 
 
